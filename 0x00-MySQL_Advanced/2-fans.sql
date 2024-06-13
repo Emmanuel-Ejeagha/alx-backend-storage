83f324dfb799cc56b475dbfeb4852a14c9525be1
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-13 21:37:00 +0100

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,11 +2,11 @@
 -- Ordered by the number of (non-unique) fans
 -- Column names must be: origin and nb_fans
 
-CREATE
-OR REPLACE
-VIEW my_v2
+-- CREATE
+-- OR REPLACE
+-- VIEW my_v2
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fabs DESC;
-SELECT * FROM my_v2;
+-- SELECT * FROM my_v2;
