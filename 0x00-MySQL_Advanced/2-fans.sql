3147f2db91010ab4c9687a14d6118493f9f017ae
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-13 21:47:45 +0100

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,11 +2,18 @@
 -- Ordered by the number of (non-unique) fans
 -- Column names must be: origin and nb_fans
 
-CREATE
-OR REPLACE
-VIEW my_v2
-SELECT origin, SUM(fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+-- Create or replace the view to aggregate and rank by fans
+CREATE OR REPLACE VIEW my_v2 AS
+SELECT 
+    origin, 
+    SUM(fans) AS nb_fans,
+    RANK() OVER (ORDER BY SUM(fans) DESC) AS rank
+FROM 
+    metal_bands
+GROUP BY 
+    origin
+ORDER BY 
+    nb_fans DESC;
+
+-- Select from the view
 SELECT * FROM my_v2;
