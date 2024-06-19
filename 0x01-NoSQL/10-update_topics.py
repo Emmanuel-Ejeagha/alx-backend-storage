e9a954feede4cc4f63ca6f17e4941a8a7059a8e4
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-19 22:55:59 +0100

    task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains a function that update a document in a collection
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """update a document into a collection"""
+    if isinstance(name, str) and all([isinstance(item, str) for item in topics]):
+        mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
